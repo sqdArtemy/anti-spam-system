f83b73712c19aa1d401c9fa97f1877aca6c20a98
RavshanMVP <93672457+RavshanMVP@users.noreply.github.com>
2024-10-12 23:21:05 +0500

    sync model and relationships between them

---
+++ bot/src/models/index.ts
@@ -0,0 +1,37 @@
+import { databaseConfig } from "./config";
+
+import { TgGroupFactory } from "./tgGroupModel";
+import { TgGroupMemberFactory } from "./tgGroupMemberModel";
+import { CheckRequestFactory } from "./checkRequestModel";
+
+export const TgGroup = TgGroupFactory(databaseConfig);
+export const TgGroupMember = TgGroupMemberFactory(databaseConfig);
+export const CheckRequest = CheckRequestFactory(databaseConfig);
+
+TgGroup.sync();
+TgGroupMember.sync();
+CheckRequest.sync();
+
+try {
+  TgGroupMember.belongsTo(TgGroup, {
+    as: "group",
+    foreignKey: "telegram_group_id",
+  });
+
+  CheckRequest.belongsTo(TgGroupMember, {
+    as: "tgGroupMember",
+    foreignKey: "telegram_group_member_id",
+  });
+
+  TgGroupMember.hasMany(CheckRequest, {
+    as: "checkRequests",
+    foreignKey: "telegram_group_member_id",
+  });
+
+  TgGroup.hasMany(TgGroupMember, {
+    as: "members",
+    foreignKey: "telegram_group_id",
+  });
+} catch (e) {
+  console.log(e);
+}
